49825452d704356669953eafd4652d55608eb0b6
dmitry shirokov <dimandima1@gmail.com>
2020-11-20 19:15:53 +0300

    1

--- treasure_game/accounts/urls.py
+++ treasure_game/accounts/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url
+from django.conf.urls import include
+from django.urls import path
 from . import views
 
 urlpatterns = [
     url(r'^login/$', views.user_login, name='login'),
+    url(r'login/', include('gameapp.urls')),
     url(r'^registration/$', views.register, name='register'),
 ]
